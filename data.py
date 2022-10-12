1abfe0e1cac73735d9c175c5b4d78f171f16ab23
Energrider <114050576+Energrider@users.noreply.github.com>
2022-10-12 08:12:55 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,22 @@
+name = 'ana'
+age = '10'
+height = '1.19'
+has_laptop = True
+
+names = ['ana 1', 'ana 2', 'sus', 'the']
+
+ages = [15, 16 ,18 ,15]
+
+#ana 1 is 15 yrs old
+#ana 2 is 16 yrs old
+#sus is 18 yrs old
+#the is 15 yrs old
+
+#for i in range(len(names)):
+#    print(f'{names[i]} is currently {ages[i]} years old!')
+
+#for currentname in names:
+# print(f'{names[0]} is {ages[0]} years old.')
+
+for i in range(len(names)):
+    print(f'{names[i]} has {len(names[i])} letters in the name.\n ugly^')
